7ce68bd2d7bd4a05766d3315804816e040a03ac5
Gnome <68836321+gnomelabs@users.noreply.github.com>
2021-07-17 19:38:47 -0400

    cleaned up scene component

--- components/Scene/index.tsx
+++ components/Scene/index.tsx
@@ -1,74 +1,84 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Scene.module.css";
+import { Loader } from "@components";
 import { useMouseEvent, useScene } from "@hooks";
 import { Oracle, Torus } from "@models";
 import { ILoadedObject, IThreeScene } from "@types";
 import { mutateStateArray } from "@utils";
-import { Loader } from "@components";
 
 interface ISceneObject {
   name: string;
   loaded: boolean;
+  setLoaded: (value: boolean) => void;
 }
 
-const objectMap: { [objectName: string]: React.FC<ILoadedObject> } = {
-  'head': (props) => <Oracle {...props} />,
-  'torus': (props) => <Torus {...props} />
+const objectsMap: {
+  [objectName: string]: any
+} = {
+  'oracle': Oracle,
+  'torus': Torus
 }
 
-const Scene: React.FC<{ objects: string[]; }> = ({ objects: objectNames }): JSX.Element => {
+const useVerifyLoaded = (objectNames: string[], sceneComponents: IThreeScene): {
+  loading: boolean,
+  objectsList: ISceneObject[] | null
+} => {
   const [loading, setLoading] = useState<boolean>(true);
-  const [objects, setObjects] = useState<React.FC<ILoadedObject>[] | null>(null);
   const [objectsList, setObjectsList] = useState<ISceneObject[] | null>(null);
-  const [sceneRef, createSceneRef] = useState<HTMLDivElement | null>(null);
-  const sceneComponents: IThreeScene = useScene(sceneRef);
-  useMouseEvent(sceneComponents);
   useEffect(() => {
-    const array = objectNames.map((name: string) => ({
+    const array = objectNames.map((name: string): ISceneObject => ({
       name,
-      loaded: false
+      loaded: false,
+      setLoaded: (value: boolean): void => {
+        setObjectsList(mutateStateArray((array) => {
+          const index = array.findIndex(obj => obj.name === name);
+          return array.splice(index, 1, {
+            name,
+            loaded: value
+          });
+        }));
+      }
     }));
     setObjectsList(array);
   }, [objectNames]);
-  const isReady = useMemo(() => {
-    return (sceneComponents && objectsList?.every(obj => obj.loaded));
-  }, [sceneComponents, objectsList]);
   useEffect(() => {
+    if (!sceneComponents || !objectsList) return;
+    const isReady = objectsList.every(obj => obj.loaded);
     if (isReady) {
       setTimeout(() => {
         setLoading(false);
       }, 3000);
     }
-  }, [isReady]);
-  const { scene, camera, renderer } = sceneComponents;
-  useEffect(() => {
-    if (!objectsList || !sceneComponents) return;
-    if (!(scene && camera && renderer)) return;
-    if (objects) return;
-    const loadObjects = (): React.FC<ILoadedObject>[] | null => {
-      return objectsList.map(({ name, loaded }): any => {
-        if (loaded) return null;
-        const setLoaded = (value: boolean): void => {
-          setObjectsList(mutateStateArray((array) => {
-            const index = array.findIndex(obj => obj.name === name);
-            return array.splice(index, 1, {
-              name,
-              loaded: value
-            });
-          }));
-        }
-        return objectMap[name]({ key: name, sceneComponents, setLoaded });
-      });
-    }
-    setObjects(loadObjects());
-  }, [objects, objectsList, sceneComponents]);
+  }, [sceneComponents, objectsList]);
+  return {
+    loading,
+    objectsList
+  }
+}
+
+const Scene: React.FC<{ objects: string[]; }> = ({ objects }): JSX.Element => {
+  const [sceneRef, createSceneRef] = useState<HTMLDivElement | null>(null);
+  const sceneComponents: IThreeScene = useScene(sceneRef);
+  const { loading, objectsList } = useVerifyLoaded(objects, sceneComponents);
+  useMouseEvent(sceneComponents);
+  const include = (objectName: string): JSX.Element | null => {
+    if (!objectsList) return null;
+    const shouldInclude = objectsList.some((object: ISceneObject): boolean => object.name === objectName);
+    const Element = objectsMap[objectName];
+    const object = (
+      <Element {...{
+        key: objectName,
+        sceneComponents,
+        setLoaded: objectsList!.find(object => object.name === objectName)!.setLoaded
+      }} />
+    )
+    return shouldInclude ? object : null;
+  }
   return (
-    <>
-      <div ref={createSceneRef} className={`${styles.Scene} ${loading ? styles.loading : ''}`}>
-        {loading && <Loader />}
-        {objects}
-      </div>
-    </>
+    <div ref={createSceneRef} className={`${styles.Scene} ${loading ? styles.loading : ''}`}>
+      {loading && <Loader />}
+      {include('oracle')}
+    </div>
   )
 }
 
